a54ac15c1a3ab655a0d34f3c0c33abae6383cd75
Dext7 <creamfields44@gmail.com>
2020-02-23 16:57:47 -0600

    Add url images

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Text('I Am Rich'),
+            backgroundColor: Colors.blueGrey[900]
+          ),
+          body: Center(
+            child: new Image(
+              image: NetworkImage('https://icons.veryicon.com/png/Business/Flat%20Finance/diamond.png')
+            ),
+          ),
+        ),
+        debugShowCheckedModeBanner: false,
       );
   }
 }
