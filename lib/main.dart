4a7a350b5cc81f0041a9423e8a2da614358ca279
Dext7 <creamfields44@gmail.com>
2020-02-23 22:22:54 -0600

    Row & Column

--- lib/main.dart
+++ lib/main.dart
@@ -10,19 +10,31 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.only(left: 30.0),
-            padding: EdgeInsets.all(20.0),
-            /*
-            margin: EdgeInsets.fromLTRB(100.0, 50.0, 50.0, 35.0),
-            margin: EdgeInsets.symmetric(
-              vertical: 50.0,
-              horizontal: 10.0
-            ), */
-            color: Colors.white,
-            child: Text('data'),
+          child: Row( // Row to Column
+            // crossAxisAlignment: CrossAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                // width: double.infinity,
+                color: Colors.white,
+                child: Text('Container'),
+              ),
+              SizedBox(height: 20.0,),
+              Container(
+                height: 100.0,
+                // width: double.infinity,
+                color: Colors.blue,
+                child: Text('Container 2')
+              ),
+              SizedBox(height: 20.0,),
+              Container(
+                height: 100.0,
+                // width: double.infinity,
+                color: Colors.amber,
+                child: Text('Container 3')
+              ),
+            ],
           )
         )
       ),
