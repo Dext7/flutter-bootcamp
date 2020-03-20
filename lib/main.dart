6bd52e183d368b5428e48c5e94986611208d2982
Dezter <creamfields44@gmail.com>
2020-03-20 00:32:25 -0600

    Expanded & crossAxisAlignment

--- lib/main.dart
+++ lib/main.dart
@@ -22,55 +22,70 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           child: SafeArea(
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  FlatButton(
-                    color: Colors.redAccent,
+                  Expanded(
+                    child: FlatButton(
+                      color: Colors.redAccent,
                       onPressed: () {
                         playSound(1);
                       },
                       child: null
+                    ),
                   ),
-                  FlatButton(
+                  Expanded(
+                    child: FlatButton(
                       color: Colors.deepOrangeAccent,
                       onPressed: () {
                         playSound(2);
                       },
                       child: null
+                    ),
                   ),
-                  FlatButton(
-                      color: Colors.orange,
-                      onPressed: () {
-                        playSound(3);
-                      },
-                      child: null
+                  Expanded(
+                      child: FlatButton(
+                          color: Colors.orange,
+                          onPressed: () {
+                            playSound(3);
+                          },
+                          child: null
+                      ),
                   ),
-                  FlatButton(
+                  Expanded(
+                    child: FlatButton(
                       color: Colors.amberAccent,
                       onPressed: () {
                         playSound(4);
                       },
                       child: null
+                    ),
                   ),
-                  FlatButton(
+                  Expanded(
+                    child: FlatButton(
                       color: Colors.black87,
                       onPressed: () {
                         playSound(5);
                       },
                       child: null
+                    ),
                   ),
-                  FlatButton(
+                  Expanded(
+                    child: FlatButton(
                       color: Colors.indigoAccent,
                       onPressed: () {
                         playSound(6);
                       },
                       child: null
+                    ),
                   ),
-                  FlatButton(
+                  Expanded(
+                    child: FlatButton(
                       color: Colors.purple,
                       onPressed: () {
                         playSound(7);
                       },
                       child: null
+                    ),
                   ),
                 ],
               )
