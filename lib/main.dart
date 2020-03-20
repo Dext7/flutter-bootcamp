efb15699ae7df9819eb8f5a1dcfdb4cb6145ae7f
Dezter <creamfields44@gmail.com>
2020-03-20 00:16:27 -0600

    Clear Center text

--- lib/main.dart
+++ lib/main.dart
@@ -20,63 +20,61 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         body: SafeArea(
-            child: Center(
-              child: SafeArea(
-                  child: Column(
-                    children: <Widget>[
-                      FlatButton(
-                        color: Colors.redAccent,
-                          onPressed: () {
-                            playSound(1);
-                          },
-                          child: null
-                      ),
-                      FlatButton(
-                          color: Colors.deepOrangeAccent,
-                          onPressed: () {
-                            playSound(2);
-                          },
-                          child: null
-                      ),
-                      FlatButton(
-                          color: Colors.orange,
-                          onPressed: () {
-                            playSound(3);
-                          },
-                          child: null
-                      ),
-                      FlatButton(
-                          color: Colors.amberAccent,
-                          onPressed: () {
-                            playSound(4);
-                          },
-                          child: null
-                      ),
-                      FlatButton(
-                          color: Colors.black87,
-                          onPressed: () {
-                            playSound(5);
-                          },
-                          child: null
-                      ),
-                      FlatButton(
-                          color: Colors.indigoAccent,
-                          onPressed: () {
-                            playSound(6);
-                          },
-                          child: null
-                      ),
-                      FlatButton(
-                          color: Colors.purple,
-                          onPressed: () {
-                            playSound(7);
-                          },
-                          child: null
-                      ),
-                    ],
-                  )
-              ),
-            )
+          child: SafeArea(
+              child: Column(
+                children: <Widget>[
+                  FlatButton(
+                    color: Colors.redAccent,
+                      onPressed: () {
+                        playSound(1);
+                      },
+                      child: null
+                  ),
+                  FlatButton(
+                      color: Colors.deepOrangeAccent,
+                      onPressed: () {
+                        playSound(2);
+                      },
+                      child: null
+                  ),
+                  FlatButton(
+                      color: Colors.orange,
+                      onPressed: () {
+                        playSound(3);
+                      },
+                      child: null
+                  ),
+                  FlatButton(
+                      color: Colors.amberAccent,
+                      onPressed: () {
+                        playSound(4);
+                      },
+                      child: null
+                  ),
+                  FlatButton(
+                      color: Colors.black87,
+                      onPressed: () {
+                        playSound(5);
+                      },
+                      child: null
+                  ),
+                  FlatButton(
+                      color: Colors.indigoAccent,
+                      onPressed: () {
+                        playSound(6);
+                      },
+                      child: null
+                  ),
+                  FlatButton(
+                      color: Colors.purple,
+                      onPressed: () {
+                        playSound(7);
+                      },
+                      child: null
+                  ),
+                ],
+              )
+          ),
         ),
       ),
       debugShowCheckedModeBanner: false,
